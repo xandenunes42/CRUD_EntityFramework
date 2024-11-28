9ec8354eed4401b30b1b0e067cfdf341a4e9e433
Alexandre Oliveira Nunes <alexandre_nunes@Alexandre.home>
2024-11-28 05:22:09 -0300

    Resolvendo conflitos e unindo os históricos

--- obj/Debug/net8.0/EntityFramework_CRUD.AssemblyInfo.cs
+++ obj/Debug/net8.0/EntityFramework_CRUD.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EntityFramework_CRUD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00e358661af973071fb1c40718bef469aff746c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3329c1eae457515851d193dc5bbd4e36e575ff82")]
 [assembly: System.Reflection.AssemblyProductAttribute("EntityFramework_CRUD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EntityFramework_CRUD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
